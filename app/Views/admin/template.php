d3e6458c08246d844d3cd11b20d7d073f1d00154
danuri <danualbantani@gmail.com>
2024-02-03 09:25:36 +0700

    Update template.php

--- app/Views/admin/template.php
+++ app/Views/admin/template.php
@@ -107,8 +107,8 @@
                         <span class="d-flex align-items-center">
                             <img class="rounded-circle header-profile-user" src="<?= base_url()?>assets/admin/images/users/avatar-1.jpg" alt="Header Avatar">
                             <span class="text-start ms-xl-2">
-                                <span class="d-none d-xl-inline-block ms-1 fw-semibold user-name-text">Anna Adame</span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                                <span class="d-none d-xl-inline-block ms-1 fw-semibold user-name-text"><?= session('nama')?></span>
+                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text"><?= session('nip')?></span>
                             </span>
                         </span>
                     </button>
